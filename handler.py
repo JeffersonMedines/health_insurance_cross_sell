c5c2a93c066201733954a9b2042a554f0c58e0eb
“JeffersonMedines” <jeffs.medines@gmail.com>
2022-12-06 16:36:42 -0300

    handler review

--- handler.py
+++ handler.py
@@ -43,5 +43,5 @@ def health_insurance_predict( object ):
         
     
 if __name__ == '__main__':
-    port = os.environ.get( 'PORT', 5000 )
-    app.run( '0.0.0.0' )
+    porti = os.environ.get( 'PORT', 5000 )
+    app.run( host='0.0.0.0', port=porti )
